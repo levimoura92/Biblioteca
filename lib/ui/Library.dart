0dfba95a1c06d0f7f757572fde4d9ac96acd709e
levimoura92 <levidasilvamoura@gmail.com>
2018-12-26 16:50:54 -0200

    Criação do esqueleto logico do login

--- lib/ui/Library.dart
+++ lib/ui/Library.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseDatabase _db = FirebaseDatabase.instance;
 
 class Library extends StatelessWidget{
   @override
